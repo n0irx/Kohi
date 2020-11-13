76a8f16cf6b927f469def83fcfe8981565591f28
nataprawiraf <nataprawiraf@gmail.com>
2020-11-14 01:55:18 +0700

    remove bindable annotation

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/viewmodel/MainViewModel.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/viewmodel/MainViewModel.kt
@@ -4,36 +4,14 @@ import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.db.model.Author
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.db.model.Recipe
-import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.db.model.TastingNote
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.db.repository.RecipeRepository
 
 
 class MainViewModel(private val repository: RecipeRepository): ViewModel() {
-
     val recipes: LiveData<List<Recipe>> = repository.recipes
-
-    // for input data
-    @Bindable
     val inputName = MutableLiveData<String>();
-
-    @Bindable
-    val inputAuthor = MutableLiveData<Author>();
-
-    @Bindable
     val inputImageSrc = MutableLiveData<String>();
-
-    @Bindable
     val inputBrewMethod = MutableLiveData<String>();
-
-    @Bindable
-    val inputTastingNotes = MutableLiveData<List<TastingNote>>();
-
-    @Bindable
     val inputPreparationTime = MutableLiveData<Long>();
-    // end of input data
-
-
-
 }
