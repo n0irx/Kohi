06b9a72ff606b3cc637e9d8478e4afd27aaeaf23
nataprawiraf <nataprawiraf@gmail.com>
2020-11-18 09:24:58 +0700

    adding on click listener on recycler view on list recipe

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/list/ListRecipeAdapter.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/list/ListRecipeAdapter.kt
@@ -1,16 +1,17 @@
 package id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.ui.list
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.R
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.databinding.RecyclerviewListRecipeBinding
-import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.data.model.Recipe
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.data.model.RecipeWithSteps
 
 class ListRecipeAdapter (
-    private val recipesWithSteps: List<RecipeWithSteps>
+    private val recipesWithSteps: List<RecipeWithSteps>,
+    private val listener: OnRecipeCickListener
 ): RecyclerView.Adapter<ListRecipeAdapter.RecipeListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
@@ -31,7 +32,23 @@ class ListRecipeAdapter (
 
 
     inner class RecipeListViewHolder(
-        val recyclerviewListRecipeBinding: RecyclerviewListRecipeBinding
-    ): RecyclerView.ViewHolder(recyclerviewListRecipeBinding.root)
+        val recyclerviewListRecipeBinding: RecyclerviewListRecipeBinding,
+    ): RecyclerView.ViewHolder(recyclerviewListRecipeBinding.root), View.OnClickListener {
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val position: Int = adapterPosition
+            if(position != RecyclerView.NO_POSITION) {
+                listener.onRecipeClick(position)
+            }
+        }
+    }
+
+    interface OnRecipeCickListener {
+        fun onRecipeClick(position: Int)
+    }
 
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/list/ListRecipeFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/list/ListRecipeFragment.kt
@@ -6,16 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.R
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.data.RecipeDatabase
+import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.data.model.RecipeWithSteps
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.data.repository.RecipeRepository
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.ui.create.CreateRecipeActivity
 import kotlinx.android.synthetic.main.fragment_list_recipe.*
 
-class ListRecipeFragment : Fragment() {
+class ListRecipeFragment : Fragment(), ListRecipeAdapter.OnRecipeCickListener {
 
     private lateinit var factory: ListRecipeViewModelFactory
     private lateinit var viewModel: ListRecipeViewModel
@@ -37,7 +39,7 @@ class ListRecipeFragment : Fragment() {
             rv_list_recipe.also {
                 it.layoutManager = LinearLayoutManager(requireContext())
                 it.setHasFixedSize(true)
-                it.adapter = ListRecipeAdapter(recipesWithSteps)
+                it.adapter = ListRecipeAdapter(recipesWithSteps, this)
             }
         })
 
@@ -46,5 +48,10 @@ class ListRecipeFragment : Fragment() {
             activity?.startActivity(intent)
             activity?.finish()
         })
+    }
+
+    override fun onRecipeClick(position: Int) {
+        val clickedItem: RecipeWithSteps = viewModel.recipesWithSteps.value?.get(position)!!
+        Toast.makeText(activity, "Recipe ${clickedItem.recipe.name} clicked", Toast.LENGTH_SHORT).show()
     }
 }
