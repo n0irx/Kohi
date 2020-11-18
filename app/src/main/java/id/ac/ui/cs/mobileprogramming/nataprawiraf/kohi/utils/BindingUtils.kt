918af4dae90a08cad2286c1254d905541cf1c7fd
nataprawiraf <nataprawiraf@gmail.com>
2020-11-18 19:29:53 +0700

    simplify binding util. just use glide

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/utils/BindingUtils.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/utils/BindingUtils.kt
@@ -1,27 +1,23 @@
 package id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.utils
 
+import android.icu.number.NumberFormatter.with
 import android.net.Uri
-import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
-import java.net.URI
+import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.R
 
-@BindingAdapter("image")
+@BindingAdapter("imageFromUri")
 fun loadImage(view: ImageView, url: String) {
     Glide.with(view)
-        .load(url)
+        .load(Uri.parse(url))
         .into(view)
 }
 
-@BindingAdapter("imageFromUri")
-fun loadImageFromUriString(view: ImageView, uriString: String) {
-    val uri: Uri = Uri.parse(uriString)
-    if (uriString == "") {
-        Picasso.get().load("https://www.healthifyme.com/blog/wp-content/uploads/2019/09/Black-coffee-feature-image.jpg")
-    } else {
-        Picasso.get().load(uri).into(view)
-    }
-}
+//@BindingAdapter("imageFromUri")
+//fun loadImageFromUriString(view: ImageView, uriString: String) {
+//    val uri: Uri = Uri.parse(uriString)
+//    Picasso.get().load(uri).into(view)
+//}
 
