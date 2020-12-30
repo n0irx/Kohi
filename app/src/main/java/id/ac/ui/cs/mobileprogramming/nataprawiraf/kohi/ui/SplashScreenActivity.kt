c3ef8b4b3be2f933e01beffe76fd4d1e977244bb
nataprawiraf <nataprawiraf@gmail.com>
2020-12-30 09:14:30 +0700

    add opengl manifest configuration set opengl as splashscreen

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/SplashScreenActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/SplashScreenActivity.kt
@@ -1,22 +1,60 @@
 package id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.ui
 
-import android.content.Intent
+import MyGLSurfaceView
+import android.content.*
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.R
+import androidx.appcompat.app.AlertDialog
 import id.ac.ui.cs.mobileprogramming.nataprawiraf.kohi.ui.list.ListRecipeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    private lateinit var gLView: GLSurfaceView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
 
-        Handler().postDelayed({
-            val intent = Intent(this, ListRecipeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        if(isNetworkAvailable(this)){
+
+            Handler().postDelayed({
+                val intent = Intent(this, ListRecipeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+        } else {
+            showAlertMenu()
+        }
+    }
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var activeNetworkInfo: NetworkInfo? = null
+        activeNetworkInfo = cm.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting
+    }
+
+    private fun showAlertMenu() {
+        val builder =
+                AlertDialog.Builder(this)
+
+        builder.setMessage("Kohi need Internet Connection for better user experience, please enable it")
+        builder.setTitle("Warning!")
+        builder.setCancelable(false)
+
+        builder
+            .setPositiveButton(
+                "Back",
+                DialogInterface.OnClickListener { dialog, which ->
+                    finish()
+                })
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nataprawiraf/kohi/ui/gl/MyGLRenderer.kt
@@ -0,0 +1,152 @@
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import android.opengl.Matrix
+import android.os.SystemClock
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class MyGLRenderer : GLSurfaceView.Renderer {
+    private lateinit var triangle: Triangle
+    private val rotationMatrix = FloatArray(16)
+    var colorVal = 1.0
+    private val DURATION_OF_FLASH = 500.0
+
+    override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
+        // Set the background frame color
+        GLES20.glClearColor(colorVal.toFloat(), 0.0f, 0.0f, 1.0f)
+        triangle = Triangle()
+    }
+
+    override fun onDrawFrame(unused: GL10) {
+        // Redraw background color
+        colorVal =
+            Math.sin(System.currentTimeMillis() * 2 * Math.PI / DURATION_OF_FLASH) * 0.4 + 1.0
+        val color = floatArrayOf(colorVal.toFloat(), 0.5f, 0.5f, 0.5f)
+        val time = SystemClock.uptimeMillis() % 4000L
+        val angle = 0.090f * time.toInt()
+
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0f, 0f, -1.0f)
+        // Draw triangle
+        triangle.draw(color)
+    }
+
+    override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+
+    }
+
+
+}
+
+// number of coordinates per vertex in this array
+const val COORDS_PER_VERTEX = 3
+var triangleCoords = floatArrayOf(     // in counterclockwise order:
+        0.0f, 0.622008459f, 0.0f,      // top
+        -0.5f, -0.311004243f, 0.0f,    // bottom left
+        0.5f, -0.311004243f, 0.0f      // bottom right
+)
+
+class Triangle {
+    private var mProgram: Int
+    private val vertexShaderCode =
+        "attribute vec4 vPosition;" +
+                "void main() {" +
+                "  gl_Position = vPosition;" +
+                "}"
+
+    private val fragmentShaderCode =
+        "precision mediump float;" +
+                "uniform vec4 vColor;" +
+                "void main() {" +
+                "  gl_FragColor = vColor;" +
+                "}"
+    init {
+
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram().also {
+
+            // add the vertex shader to program
+            GLES20.glAttachShader(it, vertexShader)
+
+            // add the fragment shader to program
+            GLES20.glAttachShader(it, fragmentShader)
+
+            // creates OpenGL ES program executables
+            GLES20.glLinkProgram(it)
+        }
+    }
+    // Set color with red, green, blue and alpha (opacity) value
+
+    fun loadShader(type: Int, shaderCode: String): Int {
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        return GLES20.glCreateShader(type).also { shader ->
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+        }
+    }
+    private var positionHandle: Int = 0
+    private var mColorHandle: Int = 0
+
+    private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
+    private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
+
+    fun draw(color: FloatArray) {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram)
+
+        // get handle to vertex shader's vPosition member
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition").also {
+
+            // Enable a handle to the triangle vertices
+            GLES20.glEnableVertexAttribArray(it)
+
+            // Prepare the triangle coordinate data
+            GLES20.glVertexAttribPointer(
+                    it,
+                    COORDS_PER_VERTEX,
+                    GLES20.GL_FLOAT,
+                    false,
+                    vertexStride,
+                    vertexBuffer
+            )
+
+            // get handle to fragment shader's vColor member
+            mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
+
+                // Set color for drawing the triangle
+                GLES20.glUniform4fv(colorHandle, 1, color, 0)
+            }
+
+            // Draw the triangle
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
+
+            // Disable vertex array
+            GLES20.glDisableVertexAttribArray(it)
+        }
+    }
+
+    private var vertexBuffer: FloatBuffer =
+        // (number of coordinate values * 4 bytes per float)
+        ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
+            // use the device hardware's native byte order
+            order(ByteOrder.nativeOrder())
+
+            // create a floating point buffer from the ByteBuffer
+            asFloatBuffer().apply {
+                // add the coordinates to the FloatBuffer
+                put(triangleCoords)
+                // set the buffer to read the first coordinate
+                position(0)
+            }
+        }
+}
